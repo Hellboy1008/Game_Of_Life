882e26097738fba20765dbac966ef8e89b6befc8
Stephen Lee <srnlee771@gmail.com>
2019-03-13 16:45:32 -0700

    make code more efficient

--- GameOfLifePanel.java
+++ GameOfLifePanel.java
@@ -11,30 +11,24 @@ public class GameOfLifePanel extends JPanel {
 
   private int[][] individualCells = new int[140][76];
   private static final double FIFTY_PERCENT = 0.5;
-  private static final int REPEAT_TIMER = 100;
+  private static final int REPEAT = 100;
   private static final int CELL_SIZE = 10;
-  private static final int CELL_ELEM_TWO = 2;
-  private static final int CELL_ELEM_THREE = 3;
-  private static final int CELL_ELEM_FOUR = 4;
-  private static final int CELL_ELEM_FIVE = 5;
-  private static final int CELL_ELEM_SIX = 6;
-  private static final int CELL_ELEM_SEVEN = 7;
   private static final int CELL_KILL_CONDITION = 4;
   private static final int CELL_REVIVE_CONDITION = 3;
 
   public GameOfLifePanel() {
-    setBackground(Color.orange); // background color
+    setBackground(Color.ORANGE); // background color
     Timer timer = new Timer(); // create timer object
-    timer.schedule(new determineFate(), 0, REPEAT_TIMER); // run determineFate every 100 milliseconds
+    timer.schedule(new determineFate(), 0, REPEAT); // run determineFate every 100 milliseconds
 
     // initialize individual cells
-    for (int i = 0; i < individualCells.length; i++) {
-      for (int j = 0; j < individualCells[i].length; j++) {
+    for (int row = 0; row < individualCells.length; row++) {
+      for (int col = 0; col < individualCells[row].length; col++) {
         double randomNum = Math.random();
         if (randomNum >= FIFTY_PERCENT) {
-          individualCells[i][j] = 1;
+          individualCells[row][col] = 1;
         } else {
-          individualCells[i][j] = 0;
+          individualCells[row][col] = 0;
         }
       }
     }
@@ -45,13 +39,13 @@ public class GameOfLifePanel extends JPanel {
     Graphics2D g2d = (Graphics2D) g;
 
     // draw cells
-    for (int i = 0; i < individualCells.length; i++) {
-      for (int j = 0; j < individualCells[i].length; j++) {
-        if (individualCells[i][j] == 1) {
-          g2d.setPaint(Color.white);
-          g2d.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-          g2d.setPaint(Color.black);
-          g2d.drawRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    for (int row = 0; row < individualCells.length; row++) {
+      for (int col = 0; col < individualCells[row].length; col++) {
+        if (individualCells[row][col] == 1) {
+          g2d.setPaint(Color.WHITE);
+          g2d.fillRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+          g2d.setPaint(Color.BLACK);
+          g2d.drawRect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
       }
     }
@@ -74,131 +68,44 @@ public class GameOfLifePanel extends JPanel {
 
   // checks cell status and updates the values
   private void checkCellStatus() {
-    int[] cornerCellValues = new int[3];
-    int[] sideCellValues = new int[5];
-    int[] middleCellValues = new int[8];
-
-    for (int i = 0; i < individualCells.length; i++) {
-      for (int j = 0; j < individualCells[i].length; j++) {
-        int numberOfAlive = 0;
-        if (i == 0 || i == individualCells.length - 1 || j == 0 || j == individualCells[0].length - 1) {
-          // check border blocks
-          if (i == 0 && j == 0) {
-            cornerCellValues[0] = individualCells[i + 1][j];
-            cornerCellValues[1] = individualCells[i][j + 1];
-            cornerCellValues[CELL_ELEM_TWO] = individualCells[i + 1][j + 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < cornerCellValues.length; counter++) {
-              if (cornerCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (i == 0 && j == individualCells[0].length - 1) {
-            cornerCellValues[0] = individualCells[i + 1][j];
-            cornerCellValues[1] = individualCells[i][j - 1];
-            cornerCellValues[CELL_ELEM_TWO] = individualCells[i + 1][j - 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < cornerCellValues.length; counter++) {
-              if (cornerCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (i == individualCells.length - 1 && j == 0) {
-            cornerCellValues[0] = individualCells[i - 1][j];
-            cornerCellValues[1] = individualCells[i][j + 1];
-            cornerCellValues[CELL_ELEM_TWO] = individualCells[i - 1][j + 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < cornerCellValues.length; counter++) {
-              if (cornerCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (i == individualCells.length - 1 && j == individualCells[0].length - 1) {
-            cornerCellValues[0] = individualCells[i - 1][j];
-            cornerCellValues[1] = individualCells[i][j - 1];
-            cornerCellValues[CELL_ELEM_TWO] = individualCells[i - 1][j - 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < cornerCellValues.length; counter++) {
-              if (cornerCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (i == 0) {
-            sideCellValues[0] = individualCells[i][j - 1];
-            sideCellValues[1] = individualCells[i + 1][j - 1];
-            sideCellValues[CELL_ELEM_TWO] = individualCells[i + 1][j];
-            sideCellValues[CELL_ELEM_THREE] = individualCells[i + 1][j + 1];
-            sideCellValues[CELL_ELEM_FOUR] = individualCells[i][j + 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < sideCellValues.length; counter++) {
-              if (sideCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (j == 0) {
-            sideCellValues[0] = individualCells[i - 1][j];
-            sideCellValues[1] = individualCells[i - 1][j + 1];
-            sideCellValues[CELL_ELEM_TWO] = individualCells[i][j + 1];
-            sideCellValues[CELL_ELEM_THREE] = individualCells[i + 1][j + 1];
-            sideCellValues[CELL_ELEM_FOUR] = individualCells[i + 1][j];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < sideCellValues.length; counter++) {
-              if (sideCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (i == individualCells.length - 1) {
-            sideCellValues[0] = individualCells[i][j - 1];
-            sideCellValues[1] = individualCells[i - 1][j - 1];
-            sideCellValues[CELL_ELEM_TWO] = individualCells[i - 1][j];
-            sideCellValues[CELL_ELEM_THREE] = individualCells[i - 1][j + 1];
-            sideCellValues[CELL_ELEM_FOUR] = individualCells[i][j + 1];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < sideCellValues.length; counter++) {
-              if (sideCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          } else if (j == individualCells[0].length - 1) {
-            sideCellValues[0] = individualCells[i - 1][j];
-            sideCellValues[1] = individualCells[i - 1][j - 1];
-            sideCellValues[CELL_ELEM_TWO] = individualCells[i][j - 1];
-            sideCellValues[CELL_ELEM_THREE] = individualCells[i + 1][j - 1];
-            sideCellValues[CELL_ELEM_FOUR] = individualCells[i + 1][j];
-            // count number of neighbours that are alive
-            for (int counter = 0; counter < sideCellValues.length; counter++) {
-              if (sideCellValues[counter] == 1) {
-                numberOfAlive++;
-              }
-            }
-          }
-        } else {
-          // check middle blocks
-          middleCellValues[0] = individualCells[i - 1][j - 1];
-          middleCellValues[1] = individualCells[i][j - 1];
-          middleCellValues[CELL_ELEM_TWO] = individualCells[i + 1][j - 1];
-          middleCellValues[CELL_ELEM_THREE] = individualCells[i + 1][j];
-          middleCellValues[CELL_ELEM_FOUR] = individualCells[i + 1][j + 1];
-          middleCellValues[CELL_ELEM_FIVE] = individualCells[i][j + 1];
-          middleCellValues[CELL_ELEM_SIX] = individualCells[i - 1][j + 1];
-          middleCellValues[CELL_ELEM_SEVEN] = individualCells[i - 1][j];
-          // count number of neighbours that are alive
-          for (int counter = 0; counter < middleCellValues.length; counter++) {
-            if (middleCellValues[counter] == 1) {
-              numberOfAlive++;
-            }
-          }
+    int numAlive;
+    for (int row = 0; row < individualCells.length; row++) {
+      for (int col = 0; col < individualCells[row].length; col++) {
+        numAlive = 0;
+        if (row != 0 && col != 0 && individualCells[row - 1][col - 1] == 1) {
+          numAlive++;
+        }
+        if (col != 0 && individualCells[row][col - 1] == 1) {
+          numAlive++;
+        }
+        if (row != individualCells.length - 1 && col != 0 && individualCells[row + 1][col - 1] == 1) {
+          numAlive++;
+        }
+        if (row != individualCells.length - 1 && individualCells[row + 1][col] == 1) {
+          numAlive++;
+        }
+        if (row != individualCells.length - 1 && col != individualCells[row].length - 1
+            && individualCells[row + 1][col + 1] == 1) {
+          numAlive++;
+        }
+        if (col != individualCells[row].length - 1 && individualCells[row][col + 1] == 1) {
+          numAlive++;
+        }
+        if (row != 0 && col != individualCells[row].length - 1 && individualCells[row - 1][col + 1] == 1) {
+          numAlive++;
+        }
+        if (row != 0 && individualCells[row - 1][col] == 1) {
+          numAlive++;
         }
         // kill the cell given the conditions below
-        if ((numberOfAlive <= 1 || numberOfAlive >= CELL_KILL_CONDITION) && individualCells[i][j] == 1) {
-          individualCells[i][j] = 0;
+        if ((numAlive <= 1 || numAlive >= CELL_KILL_CONDITION)) {
+          individualCells[row][col] = 0;
         }
         // revive the cell given the conditions below
-        if (numberOfAlive == CELL_REVIVE_CONDITION && individualCells[i][j] == 0) {
-          individualCells[i][j] = 1;
+        if (numAlive == CELL_REVIVE_CONDITION) {
+          individualCells[row][col] = 1;
         }
       }
     }
   }
-
 }
